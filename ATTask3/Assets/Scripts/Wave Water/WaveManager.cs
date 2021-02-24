1e6d545ac52699efeef443307cedbceb00a42790
Harry <harry3.newton@live.uwe.ac.uk>
2021-02-24 17:17:13 +0000

    Added in a new system where a box floats on a wave

---
+++ ATTask3/Assets/Scripts/Wave Water/WaveManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour
+{
+    public static WaveManager instance;
+
+    public float amplitude = 1f; // Height of the waves
+    public float length = 2f; // Length of the waves
+    public float speed = 1f; // Speed the waves move at
+    public float offset = 0f;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        else if (instance != null)
+        {
+            Destroy(this);
+        }
+    }
+
+    private void Update()
+    {
+        offset += Time.deltaTime * speed;
+    }
+
+    public float GetWaveHeight(float z)
+    {
+        return amplitude * Mathf.Sin(z / length + offset);
+    }
+}
